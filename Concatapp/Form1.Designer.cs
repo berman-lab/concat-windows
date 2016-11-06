8816cac2197c8e77a698f13f04986958958fa8e6
GuyKha <GuyKha@users.noreply.github.com>
2016-11-06 11:22:22 +0200

    Add project files.

---
+++ Concatapp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Concatapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.lbl_input = new System.Windows.Forms.Label();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(511, 163);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // lbl_input
+            // 
+            this.lbl_input.AutoSize = true;
+            this.lbl_input.Location = new System.Drawing.Point(12, 19);
+            this.lbl_input.Name = "lbl_input";
+            this.lbl_input.Size = new System.Drawing.Size(103, 13);
+            this.lbl_input.TabIndex = 1;
+            this.lbl_input.Text = "Input folder location:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtFolder.Location = new System.Drawing.Point(12, 46);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(443, 23);
+            this.txtFolder.TabIndex = 2;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(483, 43);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(95, 26);
+            this.btn_browse.TabIndex = 3;
+            this.btn_browse.Text = "Browse..";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 121);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(566, 23);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output Name:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 192);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(574, 151);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(598, 355);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lbl_input);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Concat app";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label lbl_input;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Concatapp/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace Concatapp
+{
+    public partial class Form1 : Form
+    {
+        private string[] acceptedExt = new string[4] { ".txt", ".fastq", ".zip", ".gz"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            // clearing output text box
+            txtResult.Text = "";
+            // checking if arguments exist
+            if (String.IsNullOrEmpty(txtFolder.Text) || String.IsNullOrEmpty(txtOutput.Text))
+                txtResult.Text += "Error - please supply arguments";
+            else
+            {
+                txtResult.Text += "Starting\r\n";
+                // getting files and printing
+                DirectoryInfo d = new DirectoryInfo(txtFolder.Text);
+                FileInfo[] Files = d.GetFiles();
+                string fileNames = "";
+                foreach (FileInfo file in Files)
+                {
+                    fileNames = fileNames + file.Name + " ";
+                }
+                txtResult.Text += "Concatenating: " + fileNames + "\r\n";
+                // check if files have valid extension
+                if (validate(Files))
+                {
+                    // creating temp folder
+                    string tempFolderPath = System.IO.Path.Combine(txtFolder.Text, "concatTemp");
+                    Directory.CreateDirectory(tempFolderPath);
+                    // concatenating
+                    if (Files[0].Extension.Equals(".txt") || Files[0].Extension.Equals(".fastq"))
+                    {
+                        using (FileStream outputStream = File.Create(tempFolderPath + @"\" + txtOutput.Text + Files[0].Extension))
+                        {
+                            foreach (FileInfo file in Files)
+                            {
+                                using (FileStream inputStream = file.Open(FileMode.Open))
+                                {
+                                    // Buffer size can be passed as the second argument.
+                                    inputStream.CopyTo(outputStream);
+                                }
+                                txtResult.Text += "The file " + file.Name + " has been processed.\r\n";
+                            }
+                        }
+                        // compressing 
+                        Compress(new FileInfo(tempFolderPath + @"\" + txtOutput.Text + Files[0].Extension));
+                        // moving compressed file to parent directory
+                        FileInfo compressed = new FileInfo(tempFolderPath + @"\" + txtOutput.Text + Files[0].Extension + ".gz");
+                        compressed.CopyTo(compressed.Directory.Parent.FullName + "\\" + compressed.Name);
+                    }
+                    if (Files[0].Extension.Equals(".zip"))
+                    {
+                        // extarcting
+                        foreach (FileInfo file in Files)
+                        {
+                            txtResult.Text += "Extracting + " + file.Name + "\r\n";
+                            ZipFile.ExtractToDirectory(file.FullName, tempFolderPath);
+                        }
+                        // getting temp directory file info
+                        DirectoryInfo tempD = new DirectoryInfo(tempFolderPath);
+                        FileInfo[] tempFiles = tempD.GetFiles();
+                        // concatenating
+                        using (FileStream outputStream = File.Create(tempFolderPath + @"\" + txtOutput.Text + tempFiles[0].Extension))
+                        {
+                            foreach (FileInfo file in tempFiles)
+                            {
+                                using (FileStream inputStream = file.Open(FileMode.Open))
+                                {
+                                    // Buffer size can be passed as the second argument.
+                                    inputStream.CopyTo(outputStream);
+                                }
+                                txtResult.Text += "The file " + file.FullName + " has been processed.\r\n";
+                            }
+                        }
+                        // compressing 
+                        Compress(new FileInfo(tempFolderPath + @"\" + txtOutput.Text + tempFiles[0].Extension));
+                        // moving compressed file to parent directory
+                        FileInfo compressed = new FileInfo(tempFolderPath + @"\" + txtOutput.Text + tempFiles[0].Extension + ".gz");
+                        compressed.CopyTo(compressed.Directory.Parent.FullName + @"\" + compressed.Name);
+                    }
+                    if (Files[0].Extension.Equals(".gz"))
+                    {
+                        string filesFullNames = "";
+                        // building filenames
+                        foreach (FileInfo file in Files)
+                        {   
+                            if (file != Files[Files.Length -1])
+                                filesFullNames = filesFullNames + "\"" + file.FullName + "\"" + " + ";
+                            else
+                                filesFullNames = filesFullNames + "\"" +  file.FullName + "\"" + " ";
+                        }
+                        // concatenating
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = "/C copy /b  " + filesFullNames + "\"" + txtFolder.Text + @"\" + txtOutput.Text + ".gz" + "\"";
+                        txtResult.Text += startInfo.Arguments;
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    }
+
+                    // deleting temp directory
+                    DirectoryInfo di = new DirectoryInfo(tempFolderPath);
+                    di.Delete(true);
+                }
+            }
+        }
+
+        // used to validate the input
+        private bool validate(FileInfo[] Files)
+        {
+            bool valid = true;
+            // checking if the extension is in the valid extension list
+            foreach (FileInfo file in Files)
+            {
+                if (Array.IndexOf(acceptedExt, file.Extension) == -1)
+                {
+                    valid = false;
+                    txtResult.Text += "Error - the file " + file.Name + "has invalid extension\r\n";
+                    break;
+                }
+            }
+            // continue only if we have valid extension, now checking if all extension are the same
+            if (valid)
+            {
+                // TODO:
+                valid = true;
+            }
+            return valid;
+        }
+
+        // compressing file to .gz
+        public static void Compress(FileInfo fi)
+        {
+            // Get the stream of the source file.
+            using (FileStream inFile = fi.OpenRead())
+            {
+                // Prevent compressing hidden and 
+                // already compressed files.
+                if ((File.GetAttributes(fi.FullName)
+                    & FileAttributes.Hidden)
+                    != FileAttributes.Hidden & fi.Extension != ".gz")
+                {
+                    // Create the compressed file.
+                    using (FileStream outFile =
+                                File.Create(fi.FullName + ".gz"))
+                    {
+                        using (GZipStream Compress =
+                            new GZipStream(outFile,
+                            CompressionMode.Compress))
+                        {
+                            // Copy the source file into 
+                            // the compression stream.
+                            inFile.CopyTo(Compress);
+
+                            Console.WriteLine("Compressed {0} from {1} to {2} bytes.",
+                                fi.Name, fi.Length.ToString(), outFile.Length.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
